91ae176dff296d884121808ab65e1b4ffca44c7b
Marcin Woś <pd3837@pjwstk.edu.pl>
2022-11-20 14:29:41 +0100

    4

--- skrypt.R
+++ skrypt.R
@@ -5,3 +5,10 @@ install.packages("jsonlite")
 
 library(httr)
 library(jsonlite)
+
+endpoint <-"https://api.openweathermap.org/data/2.5/weather?q=Warszawa&appid=1765994b51ed366c506d5dc0d0b07b77&units=metric"
+class(endpoint)
+
+gotewheather<-GET(endpoint)
+gotewheather
+
